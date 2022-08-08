66d53fe574b8947fa726774535a3014a581fba8e
Seihyun <37438619+seihyunJeong@users.noreply.github.com>
2022-08-08 19:23:54 +0900

    Updates

---
+++ precache-manifest.b68a363b0d99162e67da74ad75724bf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a4da2e2cbec0ce4bef8",
+    "url": "/waffle/static/css/main.bd36cafb.chunk.css"
+  },
+  {
+    "revision": "5a4da2e2cbec0ce4bef8",
+    "url": "/waffle/static/js/main.6d9294cf.chunk.js"
+  },
+  {
+    "revision": "7c95352e2d3f492b5ac2",
+    "url": "/waffle/static/js/runtime~main.3c2a61bb.js"
+  },
+  {
+    "revision": "95356cf7172045459e1c",
+    "url": "/waffle/static/css/2.f376a2a2.chunk.css"
+  },
+  {
+    "revision": "95356cf7172045459e1c",
+    "url": "/waffle/static/js/2.ba9c5c98.chunk.js"
+  },
+  {
+    "revision": "3e1785ef074e6d621e64",
+    "url": "/waffle/static/js/3.09116e02.chunk.js"
+  },
+  {
+    "revision": "34b33cb4b705fa12ab75e50b87925224",
+    "url": "/waffle/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4da72cadbf3cd94fa6d62cac901f4aca.js"
+  "/waffle/precache-manifest.b68a363b0d99162e67da74ad75724bf0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/waffle/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
